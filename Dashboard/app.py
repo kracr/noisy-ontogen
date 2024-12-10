3b2225ab88e8f7f840a17ea0a56379cd5667f5e1
Aditya Girdhar <92713380+AdityaGirdhar@users.noreply.github.com>
2024-12-11 03:31:22 +0530

    Add file download functionality

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
 from scripts.intersection import IntersectionNoiseGenerator
 import math
 
@@ -34,7 +34,7 @@ def submit_path():
 # Noise Page Route
 @app.route('/noise_page')
 def noise_page():
-    noise_types = ['Gaussian', 'Salt & Pepper', 'Poisson', 'Speckle']  # Example dropdown entries
+    noise_types = ['Intersection', 'Disjoint Axiom', 'Bottom Concept', 'Role Transitivity']  # Example dropdown entries
     file_name = os.listdir(app.config['UPLOAD_FOLDER'])[0]
     return render_template('noise_page.html', noise_types=noise_types, ontology_name=file_name)
 
@@ -46,13 +46,27 @@ def generate():
     print(f"\nSelected Noise Type: {noise_type}, Noise Level: {noise_level}\n")  # Print to console
 
     file_name = os.listdir(app.config['UPLOAD_FOLDER'])[0]
-    noise_gen = IntersectionNoiseGenerator('../temp/'+file_name)
-    n = math.floor((noise_level / 100) * len(noise_gen.intersection_classes))  # Calculate the number of noise entities to introduce
+    original_file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    noisy_file_name = f'noisy_{file_name}'
+    noisy_file_path = os.path.join(app.config['UPLOAD_FOLDER'], noisy_file_name)
 
-    noise_gen.introduce_noise(n)
-    noise_gen.save_ontology(f'../temp/noisy_{file_name}')
+    if (noise_type == 'Intersection'):
+        # Generate noisy ontology
+        noise_gen = IntersectionNoiseGenerator(original_file_path)
+        n = math.floor((int(noise_level) / 100) * len(noise_gen.intersection_classes))  # Calculate the number of noise entities to introduce
+        noise_gen.introduce_noise(n)
+        noise_gen.save_ontology(noisy_file_path)
 
-    return jsonify(success=True)
+        # Serve the noisy file for download
+        response = send_from_directory(app.config['UPLOAD_FOLDER'], noisy_file_name, as_attachment=True)
+
+        # Delete both files after download
+        os.remove(original_file_path)
+        os.remove(noisy_file_path)
+
+        return response
+    
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
